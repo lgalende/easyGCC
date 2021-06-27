2b7527741d5d4d0136b3cb6c9cfc582c997c2f89
Seba <sebitokazu@gmail.com>
2021-06-26 23:43:59 -0300

    Argumentos de compilacion

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -7,6 +7,7 @@ extern FILE *yyin;
 
 struct input_output
 {
+    int keep;
     char *input;
     char *output;
 } input_output;
@@ -48,6 +49,29 @@ void finish_parse()
     fclose(yyin);
 }
 
+static void
+usage(const char *progname)
+{
+    fprintf(stdout,
+            "Usage: %s [file.ecc] [OPTION]...\n"
+            "\n"
+            "   -k               Keeps the temporal .c file.\n"
+            "   -o <filename>    Binary/executable filename.\n"
+            "\n"
+            "Usage: %s [OPTION]...\n"
+            "   -h               Print help and exit.\n"
+            "   -v               Print version information and exit.\n"
+            "\n",
+            progname, progname);
+}
+
+static void
+version(void)
+{
+    fprintf(stdout, "easyGCC version 1.0\n"
+                    "ITBA - Automatas, Teoria de lenguajes y Compiladores -- Galende, Itokazu, Ratti, Rosenblatt\n");
+}
+
 void arg_parse(int argc, char *argv[], struct input_output *ip)
 {
     if (argc == 1)
@@ -55,27 +79,65 @@ void arg_parse(int argc, char *argv[], struct input_output *ip)
         fprintf(stderr, "Provide a file written in easyGCC language so that I can compile...\n");
         exit(1);
     }
+    //default values
+    ip->keep = 0;
     ip->input = argv[1];
     ip->output = "program.out";
     opterr = 0;
+
+    static const struct option op_list[] = {
+        {.name = "keep-temp", .has_arg = no_argument, .val = 'k'},
+        {.name = "output", .has_arg = required_argument, .val = 'o'},
+        {},
+    };
+    while (1)
+    {
+        int opt = getopt_long(argc, argv, ":o:kvh", op_list, NULL);
+        if (opt == -1)
+            break;
+        switch (opt)
+        {
+        case 'h':
+            usage(argv[0]);
+            exit(0);
+            break;
+        case 'k':
+            ip->keep = 1;
+            break;
+        case 'o':
+            ip->output = optarg;
+            break;
+        case 'v':
+            version();
+            exit(0);
+        case ':':
+            fprintf(stdout, "Option '%s' requires one argument\n", argv[optind - 1]);
+            exit(1);
+            break;
+        case '?':
+        default:
+            fprintf(stderr, "Unknown argument %s\n", argv[optind - 1]);
+            exit(1);
+        }
+    }
 }
 
-void compile_c_file(char *output_file)
+void compile_c_file(char *output_file, int keep_temp)
 {
     char command_buffer[256];
     sprintf(command_buffer, "gcc %s -o %s", TMP_FILE_NAME, output_file);
 
     int gcc_status = system(command_buffer);
 
-    // if (!preserveTmp && remove(TMP_FILE_NAME))
-    // {
-    //     printf("Error al eliminar el archivo de código intermedio.\n");
-    //     exit(1);
-    // }
+    if (!keep_temp && remove(TMP_FILE_NAME))
+    {
+        fprintf(stderr, "An error has ocurred during removal of temporal file.\n");
+        exit(1);
+    }
 
     if (gcc_status != 0)
     {
-        printf("Error al compilar el código intermedio generado.\n");
+        printf("An error has ocurred during compilation of temporal C code.\n");
         exit(1);
     }
 }
@@ -86,6 +148,6 @@ int main(int argc, char *argv[])
     initialize_yacc_input(input_output.input);
     yyparse();
     finish_parse();
-    compile_c_file(input_output.output);
+    compile_c_file(input_output.output, input_output.keep);
     return 0;
 }
